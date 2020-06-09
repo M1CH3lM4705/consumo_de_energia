84fb09ff809529476e9e7e6db3aa9ab4f6712751
Eduardo247-bit <57118657+Eduardo247-bit@users.noreply.github.com>
2020-06-09 09:51:39 -0400

    criação do formulário de preenchimento do aparelho

--- aparelhos/forms.py
+++ aparelhos/forms.py
@@ -1,3 +1,11 @@
 from django import forms
+from aparelhos.models import Aparelho
 
+#criação do formulario de preenchimento do aparelho.
+
+class AparelhoForm(forms.ModelForm):
+
+    class Meta:
+        model = Aparelho
+        fields = ['name', 'potencia',  'tempo']
 
