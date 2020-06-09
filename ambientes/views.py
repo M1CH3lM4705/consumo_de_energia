137b450295f3c10e23dad282a8f2c23aee54146a
Wesley Alves da Sailva <weslley.pvh.18@gmail.com>
2020-06-09 09:44:12 -0400

    função que vai receber os objetos da tabela ambiente e renderizar a pagina html

--- ambientes/views.py
+++ ambientes/views.py
@@ -1,1 +1,14 @@
 from django.shortcuts import render
+
+from ambientes.models import Ambiente
+
+# Create your views here.
+
+
+def listAmbiente(request):
+    ambientes = Ambiente.objects.all()
+    template_name = 'ambiente/lista_ambientes.html'
+    context = {
+        'ambientes':ambientes,
+    }
+    return render(request, template_name, context)
