e8ef8e0e76495ac9cea7f8a790ad92fc9493a6d9
Michel Matos <63553960+M1CH3lM4705@users.noreply.github.com>
2020-06-14 10:26:26 -0400

    criado signal

---
+++ consumo_de_energia/utils.py
@@ -0,0 +1,19 @@
+# SIGNALS
+from django.db.models import signals
+from django.template.defaultfilters import slugify
+
+def slug_pre_save(signal, instance, sender, **kwargs):
+    """Este signal gera um slug automaticamente. Ele verifica se ja existe um
+    artigo com o mesmo slug e acrescenta um numero ao final para evitar
+    duplicidade"""
+    if not instance.slug:
+        slug = slugify(instance.name)
+        novo_slug = slug
+        contador = 0
+
+        while sender.objects.filter(slug=novo_slug).exclude(id=instance.id).count() > 0:
+            contador += 1
+            novo_slug = '%s-%d'%(slug, contador)
+
+        instance.slug = novo_slug
+
