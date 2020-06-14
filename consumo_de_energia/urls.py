3f2cf622b3c92ebcfdc1b5a6b55eda297571ac20
Michel Matos <63553960+M1CH3lM4705@users.noreply.github.com>
2020-06-14 10:24:14 -0400

    ulr padrão

--- consumo_de_energia/urls.py
+++ consumo_de_energia/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('core.urls', namespace='core')),
+    path('', include('core.urls', namespace='core')),
     path('simulador/', include('ambientes.urls', namespace='ambientes')),
-    path('', include('aparelhos.urls', namespace='aparelhos')),
+    path('simulador/aparelhos/', include('aparelhos.urls', namespace='aparelhos')),
 ]
